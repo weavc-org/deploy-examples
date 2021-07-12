b31bac7a4e9808960e33ba9f08327e34eec2d144
chrisweaver1 <chris.weaver.work@gmail.com>
2021-07-12 12:27:32 +0000

    tidying code abit, adding env variables for config

--- services/src/logger/__main__.py
+++ services/src/logger/__main__.py
@@ -1,9 +1,10 @@
+import socket
 import flask
 from ..database import create_log, get_logs
 
 app = flask.Flask(__name__)
 
-@app.route("/logs")
+@app.route("/")
 def logs():
     message = "";
     logs = get_logs()
@@ -24,9 +25,9 @@ def log():
     if (len(host) == 0 or len(message) == 0):
         return flask.jsonify(status=400, errors=["Host and message are required parameters"]), 400
 
-    create_log(host, message)
+    id = create_log(host, message)
 
-    return flask.jsonify(status=200, message="Log addeed successfully"), 200
+    return flask.jsonify(status=200, message="Log addeed successfully", id=id, logged_by=socket.gethostname()), 200
 
 if (__name__ == "__main__"):
     app.run(host="0.0.0.0", port=5001)
--- services/src/web/__main__.py
+++ services/src/web/__main__.py
@@ -1,29 +1,39 @@
 from flask import Flask
 import socket
 import requests
+import os
 
 app = Flask(__name__)
 
+logger = os.getenv("LOGGER_ADDRESS", default="http://localhost:5001")
+address = os.getenv("LADDRESS", default="0.0.0.0")
+port = os.getenv("LPORT", default=5000)
+
 @app.route("/")
 def hello_world():
-    log_request()
+    result = log_request()
     hostname = socket.gethostname()
-    return f"<p>Hello, World!</p><p>host: {hostname}</p>"
+    return f"<p>Hello!</p><p>host: {hostname}</p><p>{result}</p>"
 
-def log_request():
+def log_request() -> str :
+    # make request to logger service
     try:
         r = requests.post(
-            "http://logger:5001/log", 
+            f"{logger}/log", 
             json={"host": socket.gethostname(), "message": "request recieved"})
 
-        if (r.status_code != 200):
-            print(r.json())
+        # handle result
+        if (r.status_code == 200):
+            j = r.json()
+            r.close()
+            return f"log successful (id: {j['id']}, logged by: {j['logged_by']})"
         else:
-            print("log was successful")
-        
-        r.close()
+            r.close()
+            return "log unsuccessful"
+
     except Exception:
         print("an error occured while sending the log")
+        return "log unsuccessful"
 
 if (__name__ == "__main__"):
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host=address, port=port)
