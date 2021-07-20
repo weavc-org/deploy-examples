ea733cbe41dcc81e10d10f61d24b99299583fde2
chrisweaver1 <chris.weaveer.work@gmail.com>
2021-07-20 20:56:22 +0000

    tidying up the python services

---
+++ services/web/__main__.py
@@ -0,0 +1,38 @@
+import socket
+import flask
+from core.config import SharedConfig
+from core.client.logger import log_request, get_logs_request
+
+app = flask.Flask(__name__)
+config = SharedConfig()
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    errors = []
+    if (flask.request.method == 'POST'):
+        message = flask.request.form['message']
+        result = log_request(message)
+
+        if (result[0]):
+            success = f'Log successful (id: {result[1]["id"]}, logged by: {result[1]["logged_by"]})'
+            return flask.render_template('log.html', success=success, hostname=socket.gethostname(), logs=get_logs())
+        else:
+            errors.append('An unexpected error occured while submitting the log')
+    else:
+        return flask.render_template('log.html', hostname=socket.gethostname(), logs=get_logs())
+
+    return flask.render_template('log.html', errors=errors, hostname=socket.gethostname(), logs=get_logs())    
+
+@app.route('/config')
+def get_config():
+    return config.to_json()
+
+def get_logs() -> list:
+    logsResult = get_logs_request()
+    if (logsResult[0]):
+        return logsResult[1]['logs']
+    else: 
+        return []
+
+if (__name__ == '__main__'):
+    app.run(host=config.web.address, port=config.web.port)
